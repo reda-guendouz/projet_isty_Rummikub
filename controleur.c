2bd473c14f249e5541ab78e35dd865048ed76e54
Marwane Bahraoui <bahraoui.marwane@gmail.com>
2021-01-29 11:57:25 +0100

    update controleur - CODE FONCTIONNEL

--- controleur.c
+++ controleur.c
@@ -7,6 +7,22 @@ int main(int argc, char const *argv[])
     init_joueurs(2);
     init_graphics();
 
+ 
+    POINT carre1,carre2,l1,l2;
+    carre1.x = carre1.y = 20;
+    carre2.x = carre2.y = 40;
+    l1.x = 75; l2.x = 0;
+    l1.y = 75; l2.y = 0;
+    draw_fill_rectangle(carre1,carre2,blanc);
+    int i,j;
+    for(i=0;i<8;i++){
+        for(j=0;j<30;j++){
+            load_img("assets/Tuiles/O/13.png",l1,l2);l1.x +=50;
+        }
+        l1.x=75;
+        l1.y+=75;
+    }
+    affiche_all();
 
 
     wait_escape();
